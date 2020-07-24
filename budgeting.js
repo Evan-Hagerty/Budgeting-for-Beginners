d8559ae432edcb1a8b2ddffa9bc582d80d9d78ea
Evan Hagerty <evan.hagerty3@gmail.com>
2020-07-24 16:57:30 -0400

    beginning phase

--- budgeting.js
+++ budgeting.js
@@ -1,1 +1,9 @@
+var visible=false;
+function ToggleNavItems(){
+   visible = !visible;
+    let navItems = document.querySelectorAll('nav > a');
+    for(let i=0; i < navItems.length; i++){
+        navItems[i].style.display = visible ? 'inherit' : 'none';
+    }
+}
 
