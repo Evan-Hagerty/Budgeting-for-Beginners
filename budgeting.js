080c9700de447af95ae1e955057fd254c0bdb848
Evan Hagerty <evan.hagerty3@gmail.com>
2020-07-25 00:48:30 -0400

    work in progress

--- budgeting.js
+++ budgeting.js
@@ -1,9 +1,18 @@
-var visible=false;
-function ToggleNavItems(){
-   visible = !visible;
-    let navItems = document.querySelectorAll('nav > a');
-    for(let i=0; i < navItems.length; i++){
-        navItems[i].style.display = visible ? 'inherit' : 'none';
-    }
-}
+// var visible=false;
+// function ToggleNavItems(){
+//    visible = !visible;
+//     let navItems = document.querySelectorAll('nav > a');
+//     for(let i=0; i < navItems.length; i++){
+//         navItems[i].style.display = visible ? 'inherit' : 'none';
+//     }
+// }
 
+console.log("hi");
+
+ function calculateIncome(){
+    var monthlyIncome = document.getElementById('Income').value;
+    var necessities = (monthlyIncome * .5);
+    var wants = (monthlyIncome * .3);
+    var savings = (monthlyIncome * .2); 
+    return;
+}
