6ce7e1c33dc64405df54a444e3d11cf78edb46af
Evan Hagerty <evan.hagerty3@gmail.com>
2020-07-26 21:24:14 -0400

    columns in body section

--- budgeting.js
+++ budgeting.js
@@ -1,11 +1,11 @@
-// var visible=false;
-// function ToggleNavItems(){
-//    visible = !visible;
-//     let navItems = document.querySelectorAll('nav > a');
-//     for(let i=0; i < navItems.length; i++){
-//         navItems[i].style.display = visible ? 'inherit' : 'none';
-//     }
-// }
+var visible=false;
+function ToggleNavItems(){
+   visible = !visible;
+    let navItems = document.querySelectorAll('nav > a');
+    for(let i=0; i < navItems.length; i++){
+        navItems[i].style.display = visible ? 'inherit' : 'none';
+    }
+}
 
 console.log("hi");
 
