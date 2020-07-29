681161bc47bf56e6094749825961f710d1b3fa09
Evan Hagerty <evan.hagerty3@gmail.com>
2020-07-28 20:18:51 -0400

    fixed links and formatting

--- budgeting.js
+++ budgeting.js
@@ -1,12 +1,3 @@
-var visible=false;
-function ToggleNavItems(){
-   visible = !visible;
-    let navItems = document.querySelectorAll('nav > a');
-    for(let i=0; i < navItems.length; i++){
-        navItems[i].style.display = visible ? 'inherit' : 'none';
-    }
-}
-
 console.log("hi");
 
  function calculateIncome(){
