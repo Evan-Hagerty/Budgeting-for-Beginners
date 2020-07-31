9b16b831d47a30c7a7ac14c5d7dccda40db64835
Evan Hagerty <evan.hagerty3@gmail.com>
2020-07-30 22:49:36 -0400

    added alerts to form

--- budgeting.js
+++ budgeting.js
@@ -1,13 +1,17 @@
 function calculateIncome(){
     var monthlyIncome = parseInt(document.budgeting.income.value);
-        if (monthlyIncome >= 0 && monthlyIncome <= 1000000) {
+        if (monthlyIncome > 0 && monthlyIncome <= 1000000) {
             var nec = monthlyIncome * .5;
             var want = monthlyIncome * .3;
             var saving = monthlyIncome * .2; 
             document.getElementById("necessities").value = nec;
             document.getElementById("wants").value = want;
             document.getElementById("savings").value = saving;
-        } 
+        } else if(monthlyIncome <= 0) { 
+            alert("You can't create a budget with nothing.");
+        } else if (monthlyIncome > 1000000) {
+            alert("Congratulations, with that kind of Income get a financial advisor NOW!!");
+        };
 };
 
 // Adds collapse to nav bar
