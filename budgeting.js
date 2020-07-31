2578edb43bbe2a6d5844a864429d8effccee153a
Evan Hagerty <evan.hagerty3@gmail.com>
2020-07-30 22:35:00 -0400

    added parameters to input for max income

--- budgeting.js
+++ budgeting.js
@@ -1,13 +1,13 @@
 function calculateIncome(){
     var monthlyIncome = parseInt(document.budgeting.income.value);
-        if (monthlyIncome >= 0) {
+        if (monthlyIncome >= 0 && monthlyIncome <= 1000000) {
             var nec = monthlyIncome * .5;
             var want = monthlyIncome * .3;
             var saving = monthlyIncome * .2; 
             document.getElementById("necessities").value = nec;
             document.getElementById("wants").value = want;
             document.getElementById("savings").value = saving;
-        }
+        } 
 };
 
 // Adds collapse to nav bar
